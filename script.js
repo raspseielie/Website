0d090fae49700aaa46708a85697d6e35cb358854
raspseielie <GitHub@raspseielie.site>
2025-07-31 12:47:11 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-function changePage(page) {
-  const frame = document.getElementById('main-frame');
-  const sound = document.getElementById('shutter-sound');
-  sound.currentTime = 0;
-  sound.play();
-
-  // Add delay for realism
-  setTimeout(() => {
-    frame.src = 'pages/' + page;
-  }, 200);
+function loadPage(page) {
+  document.getElementById('viewer').src = 'pages/' + page;
 }
